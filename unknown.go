d2ee43f33a6d0d0aa94cd5cd8d7267d834aed924
Robert Wallis <smilingrob@gmail.com>
2017-01-28 03:40:38 +0800

    check an unknown file format for hashes

--- unknown.go
+++ unknown.go
@@ -14,38 +14,68 @@ type HashLocation struct {
 	Line       string
 }
 
-func LoadUnknownForHashes(filename string) (result map[string]HashLocation, err error) {
-	//lineNumber := 0
+func LoadUnknownForHashes(filename string) (map[string]HashLocation, error) {
+	lineNumber := 0
 	file, err := os.Open(filename)
 	if err != nil {
 		return nil, fmt.Errorf("Couldn't scan file for hashes %v: %v", filename, err)
 	}
+	defer file.Close()
+
+	result := make(map[string]HashLocation)
 
 	rd := bufio.NewReader(file)
+	line := []rune{}
+	i := 0
 	for {
-		_, err = rd.ReadString('\n')
+		r, c, err := rd.ReadRune()
+		i += c
 		if err != nil && err != io.EOF {
 			err = fmt.Errorf("Error scanning for hashes in %v: %v", filename, err)
-			return
+			return nil, err
 		}
 		if err == io.EOF {
 			break
 		}
+		if r != '\n' {
+			if r != 0 { // utf16 hack
+				line = append(line, r)
+			}
+			continue
+		}
+		if md5 := MD5InString(line); md5 != nil {
+			lower := strings.ToLower(*md5)
+			hl := HashLocation{
+				LineNumber: lineNumber,
+				Line:       string(line),
+			}
+			result[lower] = hl
+		}
+		if sha1 := SHA1InString(line); sha1 != nil {
+			lower := strings.ToLower(*sha1)
+			hl := HashLocation{
+				LineNumber: lineNumber,
+				Line:       string(line),
+			}
+			result[lower] = hl
+		}
+		line = []rune{}
+		lineNumber++
 	}
-	return
+	return result, nil
 }
 
 // returns the substring that looks like an MD5 sum in a string
-func MD5InString(line string) *string {
+func MD5InString(line []rune) *string {
 	return hexString(line, 32)
 }
 
 // returns the substring that looks like a SHA1 sum in a string
-func SHA1InString(line string) *string {
+func SHA1InString(line []rune) *string {
 	return hexString(line, 40)
 }
 
-// returns true if the rune is a valid hexidecimal character
+// returns true if the rune is a valid hexadecimal character
 func hexRune(r rune) bool {
 	if (r >= '0' && r <= '9') || (r >= 'a' && r <= 'f') || (r >= 'A' && r <= 'F') {
 		return true
@@ -54,31 +84,24 @@ func hexRune(r rune) bool {
 }
 
 // search a string for a specific length of hex characters
-func hexString(line string, length int) *string {
+func hexString(line []rune, length int) *string {
 	start := 0
 	count := 0
-	i := 0
-	rd := strings.NewReader(line)
-	for {
-		ch, _, err := rd.ReadRune()
-		if err != nil {
-			break
-		}
+	result := ""
+	for l := range line {
+		ch := line[l]
 		if hexRune(ch) {
 			count++
 			if count == length {
-				ch, _, err := rd.ReadRune()
-				if err != nil || !hexRune(ch) {
-					result := line[start: start + length]
+				if l == len(line)-1 || !hexRune(line[l+1]) {
+					result = string(line[start : start+length])
 					return &result
 				}
-				rd.UnreadRune()
 			}
 		} else {
-			start = i + 1
+			start = l + 1
 			count = 0
 		}
-		i++
 	}
 	return nil
 }
--- unknown_test.go
+++ unknown_test.go
@@ -7,19 +7,6 @@ import (
 	"testing"
 )
 
-func Test_loadUnknown_BadFile(t *testing.T) {
-	// GIVEN a bad filename
-	fileName := "noexist"
-
-	// WHEN trying to open it
-	_, err := LoadUnknownForHashes(fileName)
-
-	// THEN it should error
-	if err == nil {
-		t.Errorf("Should have errored about the file not existing. %v", err)
-	}
-}
-
 func Test_hexRune(t *testing.T) {
 	// GIVEN a list of valid chars
 	valid := "0123456789abcdefABCDEF"
@@ -51,7 +38,7 @@ func Test_MD5InString(t *testing.T) {
 	valid := "0123456789abcdefABCDEF0123456789"
 	// WHEN md5String is called
 	// THEN it should return that md5
-	if val := MD5InString(valid); val == nil || *val != valid {
+	if val := MD5InString([]rune(valid)); val == nil || *val != valid {
 		if val != nil {
 			t.Errorf("Expecting to find the md5 sum, found \"%v\"", *val)
 		} else {
@@ -62,7 +49,7 @@ func Test_MD5InString(t *testing.T) {
 	// WHEN it's at the beginning of the string
 	// THEN it should find it
 	beginning := valid + " something else"
-	if val := MD5InString(beginning); val == nil || *val != valid {
+	if val := MD5InString([]rune(beginning)); val == nil || *val != valid {
 		if val != nil {
 			t.Errorf("Expecting to find the md5 sum, found \"%v\"", *val)
 		} else {
@@ -73,7 +60,19 @@ func Test_MD5InString(t *testing.T) {
 	// WHEN it's at the end of the string
 	// THEN it should find it
 	end := "something else " + valid
-	if val := MD5InString(end); val == nil || *val != valid {
+	if val := MD5InString([]rune(end)); val == nil || *val != valid {
+		if val != nil {
+			t.Errorf("Expecting to find the md5 sum, found \"%v\"", *val)
+		} else {
+			t.Error("Didn't find the md5 sum.")
+		}
+	}
+
+}
+
+func Test_MD5InString_Unicode(t *testing.T) {
+	u := "0CC175B9C0F1B6A831C399E269772661"
+	if val := MD5InString([]rune(u)); val == nil || *val != u {
 		if val != nil {
 			t.Errorf("Expecting to find the md5 sum, found \"%v\"", *val)
 		} else {
@@ -86,26 +85,26 @@ func Test_MD5InString_missing(t *testing.T) {
 	// GIVEN a string with something that is too short to be an md5
 	// WHEN md5String is called
 	// THEN it should return that md5
-	if val := MD5InString("0123456789abcdefABCDEF012345678"); val != nil {
+	if val := MD5InString([]rune("0123456789abcdefABCDEF012345678")); val != nil {
 		t.Errorf("Should have found nothing, found %v", *val)
 	}
-	if val := MD5InString("a 0123456789abcdefABCDEF012345678"); val != nil {
+	if val := MD5InString([]rune("a 0123456789abcdefABCDEF012345678")); val != nil {
 		t.Errorf("Should have found nothing, found %v", *val)
 	}
-	if val := MD5InString("0123456789abcdefABCDEF012345678 a"); val != nil {
+	if val := MD5InString([]rune("0123456789abcdefABCDEF012345678 a")); val != nil {
 		t.Errorf("Should have found nothing, found %v", *val)
 	}
 
 	// GIVEN a string with something too long to be an md5
 	// WHEN md5String is called
 	// THEN it should return that md5
-	if val := MD5InString("0123456789abcdefABCDEF0123456789a"); val != nil {
+	if val := MD5InString([]rune("0123456789abcdefABCDEF0123456789a")); val != nil {
 		t.Errorf("Should have found nothing, found %v", *val)
 	}
-	if val := MD5InString("b 0123456789abcdefABCDEF0123456789a"); val != nil {
+	if val := MD5InString([]rune("b 0123456789abcdefABCDEF0123456789a")); val != nil {
 		t.Errorf("Should have found nothing, found %v", *val)
 	}
-	if val := MD5InString("0123456789abcdefABCDEF0123456789a b"); val != nil {
+	if val := MD5InString([]rune("0123456789abcdefABCDEF0123456789a b")); val != nil {
 		t.Errorf("Should have found nothing, found %v", *val)
 	}
 }
@@ -115,7 +114,7 @@ func Test_SHA1InString(t *testing.T) {
 	valid := "0123456789abcdefABCDEF0123456789abcdef40"
 	// WHEN sha1String is called
 	// THEN it should return that sha1
-	if val := SHA1InString(valid); val == nil || *val != valid {
+	if val := SHA1InString([]rune(valid)); val == nil || *val != valid {
 		if val != nil {
 			t.Errorf("Expecting to find the sha1 sum, found \"%v\"", *val)
 		} else {
@@ -126,7 +125,7 @@ func Test_SHA1InString(t *testing.T) {
 	// WHEN it's at the beginning of the string
 	// THEN it should find it
 	beginning := valid + " something else"
-	if val := SHA1InString(beginning); val == nil || *val != valid {
+	if val := SHA1InString([]rune(beginning)); val == nil || *val != valid {
 		if val != nil {
 			t.Errorf("Expecting to find the sha1 sum, found \"%v\"", *val)
 		} else {
@@ -137,7 +136,7 @@ func Test_SHA1InString(t *testing.T) {
 	// WHEN it's at the end of the string
 	// THEN it should find it
 	end := "something else " + valid
-	if val := SHA1InString(end); val == nil || *val != valid {
+	if val := SHA1InString([]rune(end)); val == nil || *val != valid {
 		if val != nil {
 			t.Errorf("Expecting to find the sha1 sum, found \"%v\"", *val)
 		} else {
@@ -150,26 +149,97 @@ func Test_SHA1InString_missing(t *testing.T) {
 	// GIVEN a string with something that is too short to be an sha1
 	// WHEN sha1String is called
 	// THEN it should return that sha1
-	if val := SHA1InString("0123456789abcdefABCDEF0123456789abcdef4"); val != nil {
+	if val := SHA1InString([]rune("0123456789abcdefABCDEF0123456789abcdef4")); val != nil {
 		t.Errorf("Should have found nothing, found %v", *val)
 	}
-	if val := SHA1InString("a 0123456789abcdefABCDEF0123456789abcdef4"); val != nil {
+	if val := SHA1InString([]rune("a 0123456789abcdefABCDEF0123456789abcdef4")); val != nil {
 		t.Errorf("Should have found nothing, found %v", *val)
 	}
-	if val := SHA1InString("0123456789abcdefABCDEF0123456789abcdef4 a"); val != nil {
+	if val := SHA1InString([]rune("0123456789abcdefABCDEF0123456789abcdef4 a")); val != nil {
 		t.Errorf("Should have found nothing, found %v", *val)
 	}
 
 	// GIVEN a string with something too long to be an sha1
 	// WHEN sha1String is called
 	// THEN it should return that sha1
-	if val := SHA1InString("0123456789abcdefABCDEF0123456789abcdef40a"); val != nil {
+	if val := SHA1InString([]rune("0123456789abcdefABCDEF0123456789abcdef40a")); val != nil {
 		t.Errorf("Should have found nothing, found %v", *val)
 	}
-	if val := SHA1InString("b 0123456789abcdefABCDEF0123456789abcdef40a"); val != nil {
+	if val := SHA1InString([]rune("b 0123456789abcdefABCDEF0123456789abcdef40a")); val != nil {
 		t.Errorf("Should have found nothing, found %v", *val)
 	}
-	if val := SHA1InString("0123456789abcdefABCDEF0123456789abcdef40a b"); val != nil {
+	if val := SHA1InString([]rune("0123456789abcdefABCDEF0123456789abcdef40a b")); val != nil {
 		t.Errorf("Should have found nothing, found %v", *val)
+	}
+}
+
+func Test_LoadUnknown_BadFile(t *testing.T) {
+	// GIVEN a bad filename
+	fileName := "noexist"
+
+	// WHEN trying to open it
+	_, err := LoadUnknownForHashes(fileName)
+
+	// THEN it should error
+	if err == nil {
+		t.Errorf("Should have errored about the file not existing. %v", err)
+	}
+}
+
+func Test_LoadUnknown_Powershell_MD5(t *testing.T) {
+	// GIVEN a powershell hashsum file
+	// generated with `get-filehash -algorithm md5 *.txt > powershell.md5.txt`
+	fileName := "test_data/bad_manifests/powershell.md5.txt"
+
+	// WHEN it is scanned for MD5 files
+	results, err := LoadUnknownForHashes(fileName)
+
+	// THEN it should contain the expected hashes in the right locations
+	if err != nil {
+		t.Fatal(err)
+	}
+	aSum := "0cc175b9c0f1b6a831c399e269772661"
+	v, ok := results[aSum]
+	if !ok {
+		t.Error("Didn't find the hash for a.txt")
+	} else if v.LineNumber != 3 {
+		t.Errorf("a.txt found on the wrong line number %v", v.LineNumber)
+	}
+
+	bSum := "92eb5ffee6ae2fec3ad71c777531578f"
+	v, ok = results[bSum]
+	if !ok {
+		t.Error("Didn't find the hash for b.txt")
+	} else if v.LineNumber != 4 {
+		t.Errorf("b.txt found on the wrong line number %v", v.LineNumber)
+	}
+}
+
+func Test_LoadUnknown_Powershell_SHA1(t *testing.T) {
+	// GIVEN a powershell hashsum file
+	// generated with `get-filehash -algorithm SHA1 *.txt > powershell.sha1.txt`
+	fileName := "test_data/bad_manifests/powershell.sha1.txt"
+
+	// WHEN it is scanned for SHA1 files
+	results, err := LoadUnknownForHashes(fileName)
+
+	// THEN it should contain the expected hashes in the right locations
+	if err != nil {
+		t.Fatal(err)
+	}
+	aSum := "86f7e437faa5a7fce15d1ddcb9eaeaea377667b8"
+	v, ok := results[aSum]
+	if !ok {
+		t.Error("Didn't find the hash for a.txt")
+	} else if v.LineNumber != 3 {
+		t.Errorf("a.txt found on the wrong line number %v", v.LineNumber)
+	}
+
+	bSum := "e9d71f5ee7c92d6dc9e92ffdad17b8bd49418f98"
+	v, ok = results[bSum]
+	if !ok {
+		t.Error("Didn't find the hash for b.txt")
+	} else if v.LineNumber != 4 {
+		t.Errorf("b.txt found on the wrong line number %v", v.LineNumber)
 	}
 }
