61583ba76369785ba1de966fb6d44f007a647a75
Robert Wallis <smilingrob@gmail.com>
2017-01-28 14:06:52 +0800

    Fix main to show returned errors.

--- main.go
+++ main.go
@@ -38,5 +38,8 @@ func main() {
 	flag.Parse()
 	infoLog := log.New(os.Stdout, "", 0)
 	errorLog := log.New(os.Stderr, "", 0)
-	hashFolder(g_flags.RootDir, g_flags.ManifestFilename, g_flags.UnknownFilename, infoLog, errorLog)
+	err := hashFolder(g_flags.RootDir, g_flags.ManifestFilename, g_flags.UnknownFilename, infoLog, errorLog)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
 }
